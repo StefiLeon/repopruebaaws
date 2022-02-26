720f4e2fabededce80199f022d608335403c61ce
StefiLeon <stefaniapleon@gmail.com>
2022-02-26 19:23:00 -0300

    first

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { __dirname } from './utils.js';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+app.use(express.static(__dirname+"/public"))
+
+app.get('/info', (req, res) => {
+    res.send({PORT:PORT})
+})
